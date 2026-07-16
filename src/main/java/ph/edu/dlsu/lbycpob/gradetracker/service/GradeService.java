05764ad8987f47046f3cfae06110a82cdab3d746
Hannah Gutierrez <hannah_gutierrez@dlsu.edu.ph>
2026-07-16 15:10:53 +0800

    Added imports and buildStudent in GradeService to remove error in GradeController

--- src/main/java/ph/edu/dlsu/lbycpob/gradetracker/service/GradeService.java
+++ src/main/java/ph/edu/dlsu/lbycpob/gradetracker/service/GradeService.java
@@ -1,4 +1,11 @@
 package ph.edu.dlsu.lbycpob.gradetracker.service;
 
+import jakarta.validation.Valid;
+import ph.edu.dlsu.lbycpob.gradetracker.dto.StudentFormDTO;
+import ph.edu.dlsu.lbycpob.gradetracker.model.Student;
+
 public class GradeService {
+    public Student buildStudent(@Valid StudentFormDTO dto) {
+        return null;
+    }
 }
