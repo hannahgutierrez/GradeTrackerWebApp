d91d5f0b4d6d5ae9292717785295ea72cfc42978
Hannah Gutierrez <hannah_gutierrez@dlsu.edu.ph>
2026-07-16 15:06:34 +0800

    Added Object isFull() and getCount() in StudentSessionRepository to remove the error in GradeController

--- src/main/java/ph/edu/dlsu/lbycpob/gradetracker/service/StudentSessionRepository.java
+++ src/main/java/ph/edu/dlsu/lbycpob/gradetracker/service/StudentSessionRepository.java
@@ -1,4 +1,13 @@
 package ph.edu.dlsu.lbycpob.gradetracker.service;
 
+import org.jspecify.annotations.Nullable;
+
 public class StudentSessionRepository {
+    public @Nullable Object isFull() {
+        return null;
+    }
+
+    public @Nullable Object getCount() {
+        return null;
+    }
 }
