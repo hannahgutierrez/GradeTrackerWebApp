4cccd580f4a0f61748ab261410e066f91df66da7
Hannah Gutierrez <hannah_gutierrez@dlsu.edu.ph>
2026-07-16 15:03:13 +0800

    Added component weights constants

--- src/main/java/ph/edu/dlsu/lbycpob/gradetracker/util/GradeConstants.java
+++ src/main/java/ph/edu/dlsu/lbycpob/gradetracker/util/GradeConstants.java
@@ -11,4 +11,10 @@ public class GradeConstants {
     public static final int    ID_DIVISOR          = 11;
     public static final int    FACULTY_THRESHOLD   = 16;
 
+    // Component weights (must sum to 1.0)
+    public static final double LAB_WEIGHT          = 0.40;
+    public static final double PARTICIPATION_WEIGHT = 0.05;
+    public static final double TEACHER_WEIGHT      = 0.05;
+    public static final double EXAM_WEIGHT         = 0.20;
+    public static final double PROJECT_WEIGHT      = 0.30;
 }
