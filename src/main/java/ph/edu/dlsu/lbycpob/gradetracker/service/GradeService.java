588b8c5e0bd3dcce0a7328638e11917ed52b6167
Hannah Gutierrez <hannah_gutierrez@dlsu.edu.ph>
2026-07-16 15:28:13 +0800

    Added import and Object verifyIdNumber(@Nullable Object idNumber)  in GradeService to remove error in GradeController

--- src/main/java/ph/edu/dlsu/lbycpob/gradetracker/service/GradeService.java
+++ src/main/java/ph/edu/dlsu/lbycpob/gradetracker/service/GradeService.java
@@ -1,11 +1,16 @@
 package ph.edu.dlsu.lbycpob.gradetracker.service;
 
 import jakarta.validation.Valid;
+import org.jspecify.annotations.Nullable;
 import ph.edu.dlsu.lbycpob.gradetracker.dto.StudentFormDTO;
 import ph.edu.dlsu.lbycpob.gradetracker.model.Student;
 
 public class GradeService {
     public Student buildStudent(@Valid StudentFormDTO dto) {
         return null;
+    }
+
+    public String verifyIdNumber(@Nullable Object idNumber) {
+        return "";
     }
 }
