2e684d56c4898601ecc86afc639e1699cf55cf36
Hannah Gutierrez <hannah_gutierrez@dlsu.edu.ph>
2026-07-16 15:02:44 +0800

    Added grade constants

--- src/main/java/ph/edu/dlsu/lbycpob/gradetracker/util/GradeConstants.java
+++ src/main/java/ph/edu/dlsu/lbycpob/gradetracker/util/GradeConstants.java
@@ -1,4 +1,14 @@
 package ph.edu.dlsu.lbycpob.gradetracker.util;
 
 public class GradeConstants {
+    private GradeConstants() { }   // prevent instantiation
+
+    public static final int    MAX_STUDENTS        = 20;
+    public static final double MIN_SCORE           = 0.0;
+    public static final double MAX_SCORE           = 100.0;
+    public static final int    NUM_MODULES         = 5;
+    public static final int    ID_LENGTH           = 8;
+    public static final int    ID_DIVISOR          = 11;
+    public static final int    FACULTY_THRESHOLD   = 16;
+
 }
