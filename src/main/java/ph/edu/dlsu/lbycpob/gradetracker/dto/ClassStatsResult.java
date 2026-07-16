94713b98c802e9eb46346fb067b93f2d42501435
Hannah Gutierrez <hannah_gutierrez@dlsu.edu.ph>
2026-07-16 15:21:11 +0800

    Added setters in ClassStatsResult to remove error in GradeController

--- src/main/java/ph/edu/dlsu/lbycpob/gradetracker/dto/ClassStatsResult.java
+++ src/main/java/ph/edu/dlsu/lbycpob/gradetracker/dto/ClassStatsResult.java
@@ -1,4 +1,30 @@
 package ph.edu.dlsu.lbycpob.gradetracker.dto;
 
 public class ClassStatsResult {
+    public void setMeanRank(Object o) {
+    }
+
+    public void setClassMean(double classMean) {
+    }
+
+    public void setLowestRank(Object o) {
+    }
+
+    public void setLowestGrade(double lowest) {
+    }
+
+    public void setLowestName(String name) {
+    }
+
+    public void setHighestRank(Object o) {
+    }
+
+    public void setHighestGrade(double highest) {
+    }
+
+    public void setHighestName(String name) {
+    }
+
+    public void setTotalStudents(int size) {
+    }
 }
