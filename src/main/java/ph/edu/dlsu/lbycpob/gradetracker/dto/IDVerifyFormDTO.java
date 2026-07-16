b65c76172aef9af3558bdd83f47d7744d9282926
Hannah Gutierrez <hannah_gutierrez@dlsu.edu.ph>
2026-07-16 15:27:35 +0800

    Added Object getIdNumber() in IDVerifyFormDTO to remove error in GradeController

--- src/main/java/ph/edu/dlsu/lbycpob/gradetracker/dto/IDVerifyFormDTO.java
+++ src/main/java/ph/edu/dlsu/lbycpob/gradetracker/dto/IDVerifyFormDTO.java
@@ -1,4 +1,9 @@
 package ph.edu.dlsu.lbycpob.gradetracker.dto;
 
+import org.jspecify.annotations.Nullable;
+
 public class IDVerifyFormDTO {
+    public @Nullable Object getIdNumber() {
+        return null;
+    }
 }
